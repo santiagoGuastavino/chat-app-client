83cee53247cf30e837f9cf4529dfff073b39d738
Santiago Guastavino <san.guasta@gmail.com>
2024-02-23 15:12:00 -0300

    feat: installed and configured axios

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,9 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
+
+// aded config
 
 export const http = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BACK_URL}`,
   headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
+    "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
