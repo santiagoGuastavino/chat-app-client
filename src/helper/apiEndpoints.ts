4343395416a61928b55b75f51d07433594f9ab99
Santiago Guastavino <san.guasta@gmail.com>
2024-02-23 11:03:01 -0300

    chat-app-client version 0.0.1

--- src/helper/apiEndpoints.ts
+++
@@ -1,3 +0,0 @@
-export const apiEndpoints = [
-  'accounts', 'assets', 'customers', 'datapoints', 'devices', 'documents', 'forms', 'invites', 'media', 'messages', 'namespaces', 'orders', 'patients', 'relationships', 'rules', 'templates', 'users', 'workflows'
-]
--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-export interface ApiStatus {
-  success: boolean | undefined
-  message: string
-  hostname: string
-  time: string
-  responseStatus?: number
-  responseMessage?: string
-}
